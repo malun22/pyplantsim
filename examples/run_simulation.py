e0e4e599eabf852e1841774f62c245ac2afdb003
Luca Bernstiel <luca.bernstiel@iml.fraunhofer.de>
2025-08-14 16:57:31 +0200

    Relative path

--- examples/run_simulation.py
+++ examples/run_simulation.py
@@ -6,7 +6,7 @@ def run_model():
     with pyplantsim.Plantsim(license=pyplantsim.PlantsimLicense.RESEARCH, version="25.4",
                              visible=True, trusted=True, suppress_3d=False, show_msg_box=False) as plantsim:
 
-        plantsim.load_model(r"C:\Users\lbernsti\Documents\Projekte\Standardbibliothek\pyplantsim\examples\testModel.spp")
+        plantsim.load_model(r".\testModel.spp")
 
         plantsim.set_eventcontroller(".Models.Model.EventController")
         plantsim.start_simulation()
