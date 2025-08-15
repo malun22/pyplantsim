02d268893b08457e1f71ca6aab71860cfc26cdd3
malun22 <malun22@web.de>
2025-08-15 09:23:46 +0200

    Add a new example

--- examples/run_simulation.py
+++ examples/run_simulation.py
@@ -1,18 +1,23 @@
 import pyplantsim
 import time
+import os
 
 
 def run_model():
     with pyplantsim.Plantsim(license=pyplantsim.PlantsimLicense.RESEARCH, version="25.4",
                              visible=True, trusted=True, suppress_3d=False, show_msg_box=False) as plantsim:
+        model_path = os.path.join(os.path.dirname(__file__), "testModel.spp")
+        plantsim.load_model(model_path)
 
-        plantsim.load_model(r".\testModel.spp")
-
-        plantsim.set_eventcontroller(".Models.Model.EventController")
+        plantsim.set_event_controller(".Models.Model.EventController")
         plantsim.start_simulation()
 
         while plantsim.is_simulation_running():
             time.sleep(1)
+
+        value = plantsim.get_value_by_path('.Models.Model.DataTable["Amount",1]')
+
+        print(value)
 
 
 if __name__ == "__main__":
