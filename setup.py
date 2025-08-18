1359062bb8f5e269983172cd0bb6cf3fda50ed61
malun22 <malun22@web.de>
2025-08-18 19:19:29 +0200

    Bump the version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyplantsim',
-    version='0.1.0',
+    name="pyplantsim",
+    version="0.1.1",
     description="A Python wrapper to Siemens Tecnomatix Plant Simulation COM Interface",
     keywords=["plant", "siemens", "simulation", "COM"],
     author="Luca Bernstiel",
