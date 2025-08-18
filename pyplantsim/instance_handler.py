75cac30e1c56466428cfb8da9685e92d01d2b137
malun22 <malun22@web.de>
2025-08-18 16:12:52 +0200

    Move starting logic into init

--- pyplantsim/instance_handler.py
+++ pyplantsim/instance_handler.py
@@ -1,9 +1,10 @@
-import threading
 import queue
-from typing import Callable, Optional
+import threading
+from typing import Callable, Optional, Union
+
+from .exception import SimulationException
 from .licenses import PlantsimLicense
 from .versions import PlantsimVersion
-from .exception import SimulationException
 
 
 class InstanceHandler:
@@ -11,11 +12,45 @@ class InstanceHandler:
     Handles multiple pyplantsim workers, each with its own Plantsim instance.
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        amount_instances: int,
+        version: Union[PlantsimVersion, str] = PlantsimVersion.V_MJ_22_MI_1,
+        visible: bool = True,
+        trusted: bool = True,
+        license: Union[PlantsimLicense, str] = PlantsimLicense.VIEWER,
+        suppress_3d: bool = False,
+        show_msg_box: bool = False,
+        event_polling_interval: float = 0.05,
+        disable_log_message: bool = False,
+        simulation_finished_callback: Optional[Callable[[], None]] = None,
+        simtalk_msg_callback: Optional[Callable[[str], None]] = None,
+        fire_simtalk_msg_callback: Optional[Callable[[str], None]] = None,
+        simulation_error_callback: Optional[
+            Callable[[SimulationException], None]
+        ] = None,
+    ):
         self._job_queue = queue.Queue()
         self._shutdown_event = threading.Event()
         self._workers = []
         self._num_workers = 0
+
+        plantsim_kwargs = dict(
+            version=version,
+            visible=visible,
+            trusted=trusted,
+            license=license,
+            suppress_3d=suppress_3d,
+            show_msg_box=show_msg_box,
+            event_polling_interval=event_polling_interval,
+            disable_log_message=disable_log_message,
+            simulation_finished_callback=simulation_finished_callback,
+            simtalk_msg_callback=simtalk_msg_callback,
+            fire_simtalk_msg_callback=fire_simtalk_msg_callback,
+            simulation_error_callback=simulation_error_callback,
+        )
+
+        self._create_workers(amount_instances, **plantsim_kwargs)
 
     def __enter__(self):
         return self
@@ -23,28 +58,11 @@ class InstanceHandler:
     def __exit__(self, _, __, ___):
         self.shutdown()
 
-    def create_workers(
-        self,
-        amount_workers: int,
-        version=PlantsimVersion.V_MJ_25_MI_4,
-        visible: bool = True,
-        trusted: bool = True,
-        license: PlantsimLicense = PlantsimLicense.STUDENT,
-        suppress_3d: bool = False,
-        show_msg_box: bool = False,
-    ):
+    def _create_workers(self, amount_workers: int, **plantsim_kwargs):
         self._num_workers = amount_workers
-        plantsim_args = dict(
-            version=version,
-            visible=visible,
-            trusted=trusted,
-            license=license,
-            suppress_3d=suppress_3d,
-            show_msg_box=show_msg_box,
-        )
         for _ in range(amount_workers):
             t = threading.Thread(
-                target=self._worker, args=(plantsim_args,), daemon=True
+                target=self._worker, args=(plantsim_kwargs,), daemon=True
             )
             t.start()
             self._workers.append(t)
