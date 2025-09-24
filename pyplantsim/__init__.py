f71ee85f424024ad2938fb94010f75d0a3d062f6
malun22 <malun22@web.de>
2025-09-24 10:31:44 +0200

    add __all__

--- pyplantsim/__init__.py
+++ pyplantsim/__init__.py
@@ -2,3 +2,11 @@ from .plantsim import Plantsim
 from .exception import PlantsimException, SimulationException
 from .licenses import PlantsimLicense
 from .versions import PlantsimVersion
+
+__all__ = [
+    "Plantsim",
+    "PlantsimException",
+    "SimulationException",
+    "PlantsimLicense",
+    "PlantsimVersion",
+]
